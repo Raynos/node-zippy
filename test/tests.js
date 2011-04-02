901bc6a307cd92bd19a0ff53fe65b3e45981dd92
Joshua Holbrook <josh.holbrook@gmail.com>
2011-04-02 13:56:48 -0800

    testing the chunker

--- test/tests.js
+++ test/tests.js
@@ -6,3 +6,8 @@ exports.testZip = function() {
     assert.eql( hw.zip(['a', 'b', 'c'],[1, 2, 3]),
                   [['a', 1], ['b', 2], ['c', 3]]);
 }
+
+exports.testChunk = function() {
+    assert.eql( hw.chunk(3, [1,2,3,4,5,6,7,8,9,10]),
+                [[1,2,3],[4,5,6],[7,8,9],[10]] );
+}
