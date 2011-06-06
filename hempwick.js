31c2e0a0449b7f1d773880561e529fcced966049
Joshua Holbrook <josh.holbrook@gmail.com>
2011-06-05 17:57:46 -0800

    Removed Data.List parts from main hempwick. <_<

--- hempwick.js
+++ hempwick.js
@@ -55,31 +55,3 @@ exports.iterate = function(iterator, stop, x0) {
     }
     return xs;
 }
-
-//zippers
-exports.zipWith = function () {
-    var fxn = Array.prototype.slice.call(arguments);
-    var args = fxn.splice(1);
-    fxn = fxn[0];
-
-    var width = Math.max.apply(null, args.map(function(xs) {
-        return xs.length;
-    }));
-
-    var output = [];
-    //potentially a better way to do this?
-    for (i=0; i<width; i++) {
-        var thisOne = 
-            fxn.apply(null, args.map(function(xs) {
-                return xs[i];
-            }));
-        output.push(thisOne);
-    }
-    return output;
-}
-
-exports.zip = exports.zipWith.bind(null, function() {
-    return Array.prototype.slice.call(arguments); 
-});
-
-exports.transpose = function (xs) { return exports.zip.apply(null, xs) };
